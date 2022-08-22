910a32a041020ecd2edd833ff1393f7e9e05db9b
Sadab <sadabrahman275@gmail.com>
2022-08-22 13:24:38 +0600

    total cost added

--- js/budget.js
+++ js/budget.js
@@ -3,6 +3,20 @@ document.getElementById("calculatePlayer").addEventListener("click", function ()
     const perPlayerAmountString = perPlayerAmount.value;
     const perPlayerAmountCost = parseInt(perPlayerAmountString);
     const playerExpense = document.getElementById("intotalPlayerExpense");
-    playerExpense.innerText = perPlayerAmountCost * 5;
+    const playerCostTotal = perPlayerAmountCost * 5;
+    playerExpense.innerText = playerCostTotal;
 })
+document.getElementById("calculateTotal").addEventListener("click", function () {
+    const manager = document.getElementById("manager-amount");
+    const managerString = manager.value;
+    const managerAmount = parseInt(managerString);
 
+    const coach = document.getElementById("coach-amount");
+    const coachString = coach.value;
+    const coachAmount = parseInt(coachString);
+
+    const totalExpense = playerCostTotal + managerAmount + coachAmount;
+
+    const totalCost = document.getElementById("total-expense");
+    totalCost.innerText = totalExpense;
+})
